505a630125c130d94a0b17978ed329fec6815d8f
bruening-reply <l.bruening@reply.de>
2026-06-05 09:11:38 +0200

    add new keymaps

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -5,6 +5,10 @@ require "nvchad.mappings"
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
+map("i", "jj", "<ESC>")
+
+vim.keymap.set("n", "<leader>t", function()
+  require("nvchad.term").toggle { pos = "float" }
+end, { desc = "Toggle floating terminal" })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
