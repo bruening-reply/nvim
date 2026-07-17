0f31daa18fb9e923ae0dee3e1a9b7f5b1343f131
lbruening <l.bruening@reply.de>
2026-07-17 08:07:13 +0200

    fix: line endings bug

--- lua/options.lua
+++ lua/options.lua
@@ -1,6 +1,14 @@
 require "nvchad.options"
 
 -- add yours here!
+vim.opt.fixendofline = true
+vim.opt.endofline = true
 
+vim.api.nvim_create_autocmd("BufWritePre", {
+  callback = function()
+    vim.bo.endofline = true
+    vim.bo.fixendofline = true
+  end,
+})
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
